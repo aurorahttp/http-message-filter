88b79210d13fe69ae13308a33655a6d9b3f7b88d
panlatent <panlatent@gmail.com>
2018-01-23 15:03:03 +0800

    Fix message filter

--- src/Filter.php
+++ src/Filter.php
@@ -3,12 +3,13 @@
 namespace Aurora\Http\Message\Filter;
 
 use Aurora\Http\Handler\HandlerInterface;
+use Psr\Http\Message\MessageInterface;
 
 abstract class Filter implements FilterInterface
 {
     public function handle($request, HandlerInterface $next)
     {
-        if (! $request instanceof FilterInterface) {
+        if (! $request instanceof MessageInterface) {
             return $next->handle($request, $next);
         }
         if (method_exists($this, 'process')) {
